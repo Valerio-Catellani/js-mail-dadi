12ed282064cdcbed85fe1ffe665f35bc8537ee74
Valerio-Catellani <valeriocatellani@gmail.com>
2024-03-11 22:29:45 +0100

    added comment on code to better explain

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,50 +3,49 @@ let mail = document.getElementById("userValue");
 let userLogInResponse = document.getElementById("login-response")
 let arrayUsersMail = ["Pippo@gmail.com", "ZioLuciano@gmail.com", "SenatoreRomano@gmail.com", "Pizza@libero.it"];
 const logIn = document.getElementById("log-in");
-const playButton = document.getElementById("play-button")
+const playButton = document.getElementById("play-button") //assegno alle variabili e costanti gli elementi html
 
 mail.addEventListener("input", () => {
     if (mail.value !== "") {
         enableButtons(logIn);
     }
-});
-let checkedUser = "";
+});   //controllo che venga inserito qualcosa nell'input
+let checkedUser = ""; //dichiaro checkuser per utilizzarlo dopo nelle varie interpolazioni che richiamno il nome del player
 
 logIn.addEventListener('click', function () {
-    let inputUserMail = mail.value;
-    let userFlag = false;
-    for (let i = 0; i < arrayUsersMail.length; i++) {
-        if (arrayUsersMail[i].toLocaleLowerCase() === inputUserMail.toLocaleLowerCase()) {
+    let inputUserMail = mail.value;  //raccolgo il valore della mail
+    let userFlag = false;     //dichiaro un flag per verificare che l'utente sia registrato o meno
+    for (let i = 0; i < arrayUsersMail.length; i++) {   //semplice ciclo for
+        if (arrayUsersMail[i].toLocaleLowerCase() === inputUserMail.toLocaleLowerCase()) { //caseinsenstive?
             userFlag = true;
             checkedUser = inputUserMail.substr(0, inputUserMail.indexOf("@"))
             logIn.classList.add("d-none")
-            mail.classList.add("d-none")
-            enableButtons(playButton);
+            mail.classList.add("d-none")  //se correttamente loggato scompare la sezione del log in
+            enableButtons(playButton);  //richiamo una funzione a cui passo il parametro per abilitare il pulsante di gioco
         }
     }
     userLogInResponse.innerHTML = (userFlag === true ? `<h3>Bentornato ${checkedUser}</h3>` : '<h3>Devi registrarti per poter giocare</h3>')
-    console.log(checkedUser);
+    //saluto all'utente appena loggato, o richiesta di registrazione
 })
 
 function enableButtons(element) {
     element.classList.remove("disabled")
-}
+} //funzione utilizzata per abilitare i pulsanti
 
 
 let playerNameTab = document.querySelector("h2")
 let gameResponse = document.getElementById('game-response')
-let rollResults = document.querySelector('.results')
+let rollResults = document.querySelector('.results')  //altre variabili a cui associo elementi persi dal document
 
 playButton.addEventListener('click', function () {
-    playerNameTab.innerHTML = checkedUser;
-    gameResponse.innerHTML = "";
-    //rollResults.classList.remove("d-none");
+    playerNameTab.innerHTML = checkedUser;   //nomino la colonna di sinistra con il nome dell'utente
+    gameResponse.innerHTML = "";         //resetto il testo del risultato del lancio, qual'ora si volesse fare ulteriori tentativi
     let userRoll = getRndInteger(1, 6);
     let pcRoll = getRndInteger(1, 6);
-    rolldice(playerDice, userRoll);
+    rolldice(playerDice, userRoll);  //!Animazioni dei dadi per i miei bonus
     rolldice(pcDice, pcRoll);
-    playButton.classList.add("disabled");
-    setTimeout(() => {
+    playButton.classList.add("disabled"); //setTimeout è una funzione asincrona e così impedisco che il pulsante venga clikkato prima che il messaggio del vicnitore sia stato correttamente visualizzato
+    setTimeout(() => {  //attendo la fine delle animazioni del css di 6 secondi
         if (userRoll > pcRoll) {
             gameResponse.innerHTML = `<h3>${checkedUser} ha vinto con ${userRoll} contro il PC con ${pcRoll}</h3>`
         } else if (userRoll < pcRoll) {
@@ -54,7 +53,7 @@ playButton.addEventListener('click', function () {
         } else {
             gameResponse.innerHTML = `<h3>${checkedUser} e PC hanno pareggiato con ${pcRoll}</h3>`
         };
-        enableButtons(playButton);
+        enableButtons(playButton); //riabilito e rinomino il pulsante per ulteriori giocate
         playButton.innerHTML = "Gioca di Nuovo";
     }, 6000);
 
@@ -66,10 +65,10 @@ playButton.addEventListener('click', function () {
 */
 
 let playerDice = document.getElementById("player-dice")
-let pcDice = document.getElementById("pc-dice")
+let pcDice = document.getElementById("pc-dice")  //assegno alle variabili i dadi
 
 function rolldice(dice, result) {
-    let xRandom;
+    let xRandom;  //randomizzo le varie rotazione facendo in modo da ottenere solo dei multipli
     let yRandom;
     if (result === 1) {
         xRandom = 360 * getRndInteger(1, 24);
@@ -91,5 +90,5 @@ function rolldice(dice, result) {
         xRandom = (90) + ((360) * getRndInteger(1, 24));
         yRandom = 360 * getRndInteger(1, 24);
     }
-    dice.style.transform = `rotateX(${xRandom}deg) rotateY(${yRandom}deg) `;
+    dice.style.transform = `rotateX(${xRandom}deg) rotateY(${yRandom}deg) `; //rollo i dadi
 }
